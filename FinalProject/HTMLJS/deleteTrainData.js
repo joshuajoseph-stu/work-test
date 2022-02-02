b17c4e9d308fe1813b518775d05f8968318ac61e
Joshua Joseph <joshuajoseph1012@gmail.com>
2022-02-02 17:41:14 +0530

    fixed spacing, everything looks neat

--- FinalProject/HTMLJS/deleteTrainData.js
+++ FinalProject/HTMLJS/deleteTrainData.js
@@ -1,12 +1,11 @@
-
-const url = 'http://localhost:9191/api/train';
+const url = "http://localhost:9191/api/train";
 
 async function callApi() {
-    let response = await fetch(url);
-    let jsonStr = await response.text();
-    console.log(jsonStr);
+  let response = await fetch(url);
+  let jsonStr = await response.text();
+  console.log(jsonStr);
 
-    createTable(jsonStr);
+  createTable(jsonStr);
 }
 
 // fetch(YOUR_URL, {
@@ -18,49 +17,44 @@ async function callApi() {
 //   })
 
 function createTable(jsonStr) {
+  const jsonObj = JSON.parse(jsonStr);
 
-    const jsonObj = JSON.parse(jsonStr);
+  //  const data = jsonObj.data;
+  const data = jsonObj;
 
-    //  const data = jsonObj.data;
-    const data = jsonObj;
+  const tableBody = document.getElementById("table_body");
 
-    const tableBody = document.getElementById('table_body')
+  for (let x in data) {
+    let obj = data[x];
 
-    for (let x in data) {
+    const row = document.createElement("tr");
 
-        let obj = data[x];
+    const cell1 = document.createElement("td");
+    cell1.innerHTML = obj.trainNo;
+    const cell2 = document.createElement("td");
+    cell2.innerHTML = obj.trainName;
+    const cell3 = document.createElement("td");
+    cell3.innerHTML = obj.source;
+    const cell4 = document.createElement("td");
+    cell4.innerHTML = obj.destination;
+    const cell5 = document.createElement("td");
+    cell5.innerHTML = obj.ticketPrice;
+    const cell6 = document.createElement("td");
+    let btn = document.createElement("button");
+    btn.classList.add("btn", "btn-danger");
+    btn.innerHTML = "Delete";
 
-        const row = document.createElement('tr');
+    cell6.appendChild(btn);
 
-        const cell1 = document.createElement('td');
-        cell1.innerHTML = obj.trainNo;
-        const cell2 = document.createElement('td');
-        cell2.innerHTML = obj.trainName;
-        const cell3 = document.createElement('td');
-        cell3.innerHTML = obj.source;
-        const cell4 = document.createElement('td');
-        cell4.innerHTML = obj.destination;
-        const cell5 = document.createElement('td');
-        cell5.innerHTML = obj.ticketPrice;
-        const cell6 = document.createElement('td');
-        let btn = document.createElement('button');
-        btn.classList.add('btn','btn-danger');
-        btn.innerHTML='Delete';
+    row.appendChild(cell1);
+    row.appendChild(cell2);
+    row.appendChild(cell3);
+    row.appendChild(cell4);
+    row.appendChild(cell5);
+    row.appendChild(cell6);
 
-        cell6.appendChild(btn);
-        
-
-
-        row.appendChild(cell1);
-        row.appendChild(cell2);
-        row.appendChild(cell3);
-        row.appendChild(cell4);
-        row.appendChild(cell5);
-        row.appendChild(cell6);
-        
-
-        tableBody.appendChild(row);
-    }
+    tableBody.appendChild(row);
+  }
 }
 
 callApi();
--- FinalProject/HTMLJS/getTicketData.js
+++ FinalProject/HTMLJS/getTicketData.js
@@ -1,68 +1,64 @@
-
-const url = 'http://localhost:9191/api/ticket';
+const url = "http://localhost:9191/api/ticket";
 
 async function callApi() {
-    let response = await fetch(url);
-    let jsonStr = await response.text();
-    console.log(jsonStr);
+  let response = await fetch(url);
+  let jsonStr = await response.text();
+  console.log(jsonStr);
 
-    createTable(jsonStr);
+  createTable(jsonStr);
 }
 
 function createTable(jsonStr) {
+  const jsonObj = JSON.parse(jsonStr);
 
-    const jsonObj = JSON.parse(jsonStr);
+  //  const data = jsonObj.data;
+  const data = jsonObj;
 
-    //  const data = jsonObj.data;
-    const data = jsonObj;
+  const tableBody = document.getElementById("table_body");
 
-    const tableBody = document.getElementById('table_body')
+  for (let x in data) {
+    let obj = data[x];
 
-    for (let x in data) {
+    const row = document.createElement("tr");
 
-        let obj = data[x];
+    const cell1 = document.createElement("td");
+    cell1.innerHTML = obj.id;
+    const cell2 = document.createElement("td");
+    cell2.innerHTML = obj.pnr;
+    const cell3 = document.createElement("td");
+    cell3.innerHTML = obj.date;
+    const cell4 = document.createElement("td");
+    cell4.innerHTML = obj.trainNo;
+    const cell5 = document.createElement("td");
+    cell5.innerHTML = obj.passenger1Id;
 
-        const row = document.createElement('tr');
+    // const cell6 = document.createElement('td');
+    // const btnBook = document.createElement('button');
+    // btnBook.classList.add('btn', 'btn-primary');
+    // btnBook.innerHTML = 'Book';
 
-        const cell1 = document.createElement('td');
-        cell1.innerHTML = obj.id;
-        const cell2 = document.createElement('td');
-        cell2.innerHTML = obj.pnr;
-        const cell3 = document.createElement('td');
-        cell3.innerHTML = obj.date;
-        const cell4 = document.createElement('td');
-        cell4.innerHTML = obj.trainNo;
-        const cell5 = document.createElement('td');
-        cell5.innerHTML = obj.passenger1Id;
+    // const btnEdit = document.createElement('button');
+    // btnEdit.innerHTML = 'Edit';
+    // btnEdit.classList.add('btn', 'btn-success');
+    // btnEdit.innerHTML = 'Edit';
 
-        // const cell6 = document.createElement('td');
-        // const btnBook = document.createElement('button');
-        // btnBook.classList.add('btn', 'btn-primary');
-        // btnBook.innerHTML = 'Book';
+    // const btnDelete = document.createElement('button');
+    // btnDelete.classList.add('btn', 'btn-danger');
+    // btnDelete.innerHTML = 'Delete';
 
-        // const btnEdit = document.createElement('button');
-        // btnEdit.innerHTML = 'Edit';
-        // btnEdit.classList.add('btn', 'btn-success');
-        // btnEdit.innerHTML = 'Edit';
+    // cell6.appendChild(btnBook);
+    // cell6.appendChild(btnEdit);
+    // cell6.appendChild(btnDelete);
 
-        // const btnDelete = document.createElement('button');
-        // btnDelete.classList.add('btn', 'btn-danger');
-        // btnDelete.innerHTML = 'Delete';
+    row.appendChild(cell1);
+    row.appendChild(cell2);
+    row.appendChild(cell3);
+    row.appendChild(cell4);
+    row.appendChild(cell5);
+    // row.appendChild(cell6);
 
-        // cell6.appendChild(btnBook);
-        // cell6.appendChild(btnEdit);
-        // cell6.appendChild(btnDelete);
-
-        row.appendChild(cell1);
-        row.appendChild(cell2);
-        row.appendChild(cell3);
-        row.appendChild(cell4);
-        row.appendChild(cell5);
-        // row.appendChild(cell6);
-
-
-        tableBody.appendChild(row);
-    }
+    tableBody.appendChild(row);
+  }
 }
 
 callApi();
